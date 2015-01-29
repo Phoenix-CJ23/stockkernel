babf03727b0e6a6a6cf2ff7ba09cc8403b5d0a8a
chteow <chteow@163.com>
2015-01-29 12:43:57 +0800

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 25 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#25 SMP PREEMPT Wed Jan 28 16:03:07 CST 2015"
+#define LINUX_COMPILE_BY "chteow"
+#define LINUX_COMPILE_HOST "TPX1C"
+#define LINUX_COMPILER "gcc version 4.9.3 20141031 (prerelease) (crosstool-NG hg+default-db3f7ffabdc8 - LINARO CORE) "
---
+++ make_kernel.sh
@@ -0,0 +1,3 @@
+make ARCH=arm CROSS_COMPILE=arm-eabi- zImage-dtb -j4 
+#sleep 5
+#./scripts/dtbTool -v -s 2048 -o dt.img arch/arm/boot/
